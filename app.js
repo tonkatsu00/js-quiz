6216cbad1493e12f3c0a6605178bb8276145008d
tonkatsu00 <kyo.snnstt@outlook.jp>
2021-02-10 03:13:38 +0900

    question1

---
+++ app.js
@@ -0,0 +1,50 @@
+const question = 'ゲーム史上、最も売れたゲーム機は次のうちどれ？';
+const answers = [
+  'スーパーファミコン',
+  'プレイステーション2',
+  'ニンテンドースイッチ',
+  'ニンテンドーDS',
+];
+const correct = 'ニンテンドーDS';
+
+// 定数の文字列をHTMLに反映させる
+document.getElementById('js-question').textContent = question;
+
+const $button = document.getElementsByTagName('button');
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+$button[3].textContent = answers[3];
+
+// ボタンをクリックしたら正誤判定
+$button[0].addEventListener('click', () => {
+  if(correct === $button[0].textContent){
+    window.alert('正解！');
+  } else {
+    window.alert('不正解！');
+  }
+});
+
+$button[1].addEventListener('click', () => {
+  if(correct === $button[1].textContent){
+    window.alert('正解！');
+  } else {
+    window.alert('不正解！');
+  }
+});
+
+$button[2].addEventListener('click', () => {
+  if(correct === $button[2].textContent){
+    window.alert('正解！');
+  } else {
+    window.alert('不正解！');
+  }
+});
+
+$button[3].addEventListener('click', () => {
+  if(correct === $button[3].textContent){
+    window.alert('正解！');
+  } else {
+    window.alert('不正解！');
+  }
+});
